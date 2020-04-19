1a6265ca750eaa0dfd9c9903c7a1cb6f65924017
Deiby Mejia <dmejia1204@gmail.com>
2020-04-19 05:22:37 -0600

    creation of development environment

---
+++ src/containers/App.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<style>
+  ::global(body) {
+      /* this will apply to <body> */
+      /* your styles go here */
+      background-color: #676778;
+      color: white;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello Pugsgram!</h1>
+</div>
